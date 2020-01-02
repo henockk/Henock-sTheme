d59ef1e4b30afe3b67e340bc37b90684534bc3df
henockk <51589733+henockk@users.noreply.github.com>
2020-01-02 14:30:01 +0300

    sidebar on all pages

--- page.php
+++ page.php
@@ -14,7 +14,9 @@
 
 get_header();
 ?>
-
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-8">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -35,6 +37,11 @@ get_header();
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+<!-- ?php -->
+</div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -10,6 +10,10 @@
 get_header();
 ?>
 
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -32,6 +36,12 @@ get_header();
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+<!-- ?php -->
+
+</div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
