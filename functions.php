11d217988d0a802ab9522ce8a8fb2c468f262091
henockk <51589733+henockk@users.noreply.github.com>
2020-01-08 12:40:19 +0300

    footer and function

--- functions.php
+++ functions.php
@@ -105,6 +105,43 @@ add_action( 'after_setup_theme', 'henock_s_theme_content_width', 0 );
  */
 function henock_s_theme_widgets_init() {
 	register_sidebar( array(
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 3',
+		'id' => 'footer-sidebar-3',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 4',
+		'id' => 'footer-sidebar-4',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	
+	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'henock-s-theme' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'henock-s-theme' ),
