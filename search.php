560c9e8d2a304c90d1faf080cc0356e8bd511bff
henockk <51589733+henockk@users.noreply.github.com>
2020-01-08 12:39:15 +0300

    archive and search

--- search.php
+++ search.php
@@ -9,6 +9,9 @@
 
 get_header();
 ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -50,6 +53,11 @@ get_header();
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+	</div>
+    <div class="col-sm-4">
+    <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
+
