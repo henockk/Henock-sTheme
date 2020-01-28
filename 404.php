e0cc65d507a3c32ac8cceda0ba714e40d44a1294
henockk <51589733+henockk@users.noreply.github.com>
2020-01-28 15:52:47 +0300

    404 change

--- 404.php
+++ 404.php
@@ -18,39 +18,39 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'henock-s-theme' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Sorry! The page you are looking for can&rsquo;t be found.', 'henock-s-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'henock-s-theme' ); ?></p>
 
 					<?php
-					get_search_form();
+					// get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'henock-s-theme' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( '', 'henock-s-theme' ); ?></h2>
 						<ul>
 							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							// wp_list_categories( array(
+							// 	'orderby'    => 'count',
+							// 	'order'      => 'DESC',
+							// 	'show_count' => 1,
+							// 	'title_li'   => '',
+							// 	'number'     => 10,
+							// ) );
 							?>
 						</ul>
 					</div><!-- .widget -->
 
 					<?php
 					/* translators: %1$s: smiley */
-					$henock_s_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'henock-s-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$henock_s_theme_archive_content" );
+					// $henock_s_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'henock-s-theme' ), convert_smilies( ':)' ) ) . '</p>';
+					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$henock_s_theme_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
+					// the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
