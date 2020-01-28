705f4156ccaa0c078118b6be0f4ef9940d342c61
henockk <51589733+henockk@users.noreply.github.com>
2020-01-28 11:51:22 +0300

    small header

--- header.php
+++ header.php
@@ -42,6 +42,7 @@
 
 		<nav id="site-navigation" class="main-navigation ">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'henock-s-theme' ); ?></button>
+
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
